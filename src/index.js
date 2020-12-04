d9ec4f73cc7a8b06f974e7ea243f515a2a095835
Juan Lucas Ibarra Sierra <jl.ibarra@uniandes.edu.co>
2020-12-03 23:40:06 -0500

    Commit final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobsList";
+console.log(window.navigator.language.split("-")[0]);
+let language = "";
+if (window.navigator.language.split("-")[0] === 'es') {
+    language = localeEsMessages
+}
+else {
+    language = localeEnMessages
+}
+ReactDOM.render(
+    <IntlProvider locale={window.navigator.language.split("-")[0]} messages={language}>
+        <JobsList />
+    </IntlProvider>
+    , document.getElementById("root")
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+);
